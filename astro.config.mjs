125a2fd9fad4c156b48c86c9e16d093b5b3c3b5b
almez13 <aleksey.mezentsev@outlook.com>
2024-01-30 17:51:27 +0200

    Insert preact component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-blog-6ce3cf.netlify.app/"
-
+  site: "https://astro-blog-6ce3cf.netlify.app/",
+  integrations: [preact()]
 });
